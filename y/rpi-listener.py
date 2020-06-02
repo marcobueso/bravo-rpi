cb840af0e1297785584ff1e92da924216078df12
marcobueso <mbueso97@gmail.com>
2020-06-02 13:38:06 -0500

    Update rpi-listener.py

--- y/rpi-listener.py
+++ y/rpi-listener.py
@@ -24,6 +24,8 @@ def on_snapshot(col_snapshot, changes, read_time):
             newpid = os.fork()
             if newpid == 0:
                 child(change.document.get('original'))
+            else:
+                sleep(1)
         # elif change.type.name == 'MODIFIED':
         #     print(u'Modified city: {}'.format(change.document.id))
         # elif change.type.name == 'REMOVED':
@@ -31,7 +33,7 @@ def on_snapshot(col_snapshot, changes, read_time):
         #     delete_done.set()
 def child(message):
     print("Child executing...")
-    os.system('/home/pi/Desktop/rpi-rgb-led-matrix-master/examples-api-use/scrolling-text-example --led-rows=16 --led-cols=64 --led-no-hardware-pulse -s 3 -f /home/pi/Desktop/rpi-rgb-led-matrix-master/fonts/6x13.bdf ' + message)
+    os.system('/home/pi/Desktop/rpi-rgb-led-matrix-master/examples-api-use/scrolling-text-example --led-rows=16 --led-cols=64 --led-no-hardware-pulse -s 3 -l 1 -f /home/pi/Desktop/rpi-rgb-led-matrix-master/fonts/6x13.bdf ' + message)
 
 ####col_query = db.collection(u'cities').where(u'state', u'==', u'CA')
 col_query = db.collection(u'messages')
